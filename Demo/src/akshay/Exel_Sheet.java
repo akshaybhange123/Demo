d631aacbdfd4dc3617eb4a86f86dd8439b5ccbd9
akshay.bhange <akshay.bhange@PUNITP380195L.ad.infosys.com>
2023-06-16 10:51:52 +0530

    I am New

---
+++ Demo/src/akshay/Exel_Sheet.java
@@ -0,0 +1,23 @@
+package akshay;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Exel_Sheet 
+{
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Chromrdriver_Path\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+      //   FileInputStream File = new  FileInputStream(" ");
+      //   String info = WorkBookFactory.creat(File).getsheet("Sheet1").getRow(0).getcell(1).getStringCellvalue();
+       //  System.out.println(info);
+
+
+		
+		
+	}
+
+}
